292f2d80f47d6bfa05c88c2b2f1bae08202bbd6c
Haryz Izzudin <haryzizzudin@gmail.com>
2020-02-07 18:16:12 +0800

    Add base items for repository pattern

---
+++ templates/webapi-clean/src/CleanWebApi.Persistence/Repositories/EfRepository.cs
@@ -0,0 +1,57 @@
+﻿namespace CleanWebApi.Persistence.Repositories
+{
+    public class EfRepository<T> : IAsyncRepository<T> where T : BaseEntity
+    {
+        protected readonly RobustaDbContext _context;
+
+        public EfRepository(RobustaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> specification)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), specification);
+        }
+    }
+}
